3e3a2d8c722d22d5b2129761dcbbdb11a37b323a
Zubaydullo <zubaycoder0022@gmail.com>
2021-09-21 11:20:42 +0500

    removed whitenoise

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -64,10 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'hotel_management.urls'
 
