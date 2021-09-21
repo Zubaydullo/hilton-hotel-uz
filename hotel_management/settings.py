2110573902ba4a9e11951108ca5cf5721b64b8ab
Zubaydullo <zubaycoder0022@gmail.com>
2021-09-21 11:36:40 +0500

    statis settings

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -159,11 +159,11 @@ MESSAGE_TAGS = {
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MEDIA_URL = '/media/'
 
