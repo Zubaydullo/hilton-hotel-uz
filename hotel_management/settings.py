a0e968e176860ac89c4821caa9f7132011fd38e2
Your Name <you@example.com>
2021-12-04 22:08:30 +0500

    feat: added nothong

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
