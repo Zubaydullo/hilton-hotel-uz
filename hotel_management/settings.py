231bee5445365f37421093e2464d86f37a3a60d8
Zubaydullo Abdirakhmonov <zubaydullo0022@gmail.com>
2022-09-18 01:20:47 +0500

    troubleshooting

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -156,7 +156,7 @@ MESSAGE_TAGS = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'user/static'),
