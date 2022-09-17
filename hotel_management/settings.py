d4608021826c6eae31755f0815ec70dc25834cba
Zubaydullo Abdirakhmonov <zubaydullo0022@gmail.com>
2022-09-18 01:26:56 +0500

    troubleshooting

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -180,11 +180,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'zubaycoder0022@gmail.com'
-EMAIL_HOST_PASSWORD = '**********'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'zubaycoder0022@gmail.com'
+# EMAIL_HOST_PASSWORD = '**********'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 
 # Stripe Payment Api keys 
