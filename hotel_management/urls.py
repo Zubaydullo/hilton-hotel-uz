deccb43cc3837b31d215348edb8b684e6b984cfe
Zubaydullo Abdirakhmonov <zubaydullo0022@gmail.com>
2022-09-18 01:29:59 +0500

    troubleshooting

--- hotel_management/urls.py
+++ hotel_management/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('user.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
     path('blog/', include('blog_post.urls')),
